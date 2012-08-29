7fc2fdc8ec423c55e315e0b6db6c49a8634bd766
Andrew Grangaard <granny-github@ofb.net>
2012-08-28 23:32:27 -0700

    removes la.pm.org references from Abstract

--- lib/App/PM/Website.pm
+++ lib/App/PM/Website.pm
@@ -6,6 +6,6 @@ package App::PM::Website;
 use base 'App::Cmd';
 
 
-#ABSTRACT: tools for maintaining the los angeles perl mongers website
+#ABSTRACT: tools for maintaining the a perl mongers (webDav) website
 
 1;
